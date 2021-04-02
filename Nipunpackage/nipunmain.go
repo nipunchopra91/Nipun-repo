607020fd6c21dff9bc2496b604b11a8856e86c02
nipunchopra91 <nipun.chopra@cohesity.com>
2021-04-03 00:57:02 +0530

    move within a folder

---
+++ Nipunpackage/nipunmain.go
@@ -0,0 +1,14 @@
+package main
+
+//All imports done here
+import (
+	"fmt"
+
+	"github.com/Nipun-repo/Nipunpackage"
+
+)
+//Main function or entry point to tests
+func main(){
+	fmt.Println("Calling Nipun func from Nipunpackage")
+	Nipunpackage.Nipunfunc()
+}
